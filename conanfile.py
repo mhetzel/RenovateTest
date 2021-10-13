ab29d9990ea1a8eee6570c0abc19d3055375d08e
mhetzel <34289010+mhetzel@users.noreply.github.com>
2021-10-13 08:45:10 -0500

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,12 @@ from conans import ConanFile
 
 class Pkg(ConanFile):
    python_requires = "pybind11/0.1"  # recipe to reuse code from
-   build_requires = "toml/0.2@user/testing", "libsass/0.2@user/testing"
-   requires = "tl-function-ref/1.0", "zulu-openjdk/2.1@otheruser/testing"
+   build_requires = "toml/0.2@user/testing", 'libsass/0.2@user/testing'
+   requires = 'tl-function-ref/1.0', "zulu-openjdk/2.1@otheruser/testing"
 
    requires = [("optional-lite/0.1@user/testing"),
                ("snappy/0.2@dummy/stable", "override"),
-               ("wayland/2.1@coder/beta", "private")]
+               ('wayland/2.1@coder/beta', "private")]
 
    requires = (("libuv/1.0@user/stable", "private"), )
    requires = ('ninja/1.0@user/stable', ("tinyspline/3.0@other/beta", "override"))
